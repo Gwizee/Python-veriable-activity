9acad2bd4a7193285384640ab0be70c8276a3b57
Gwizee <risingfire69@gmail.com>
2024-09-26 21:39:05 +0600

    Vriable Activity completed

---
+++ main.py
@@ -0,0 +1,10 @@
+numb1=10 # integer
+numb2=3.4 # float
+name="Saumik" # string
+IsSaumikStudent= True # boolean
+print("Integer :",numb1)
+print("Float :",numb2)
+print("String :",name)
+print("Boolean :",IsSaumikStudent)
+username=input("Enter Your Name")
+print("Welcome",username)
